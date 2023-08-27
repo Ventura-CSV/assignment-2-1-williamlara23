15de72e796bfd16ac0c1c8ff26afbe49d41f0efe
williamlara23 <wilberth_lara1@my.vcccd.edu>
2023-08-26 17:49:57 -0700

    first commit on assignment 2-1

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
+    males = int(input("How many male students: "))
+    females = int(input("How many female students: "))
+    total = int(males)+ int(females)
+    m_perc = float(males)/total
+    f_perc = float(females)/total
+    print("The total number of students: " + str(total))
+    print("The total of males and females: " + str(males) +" "+str(females))
+    print(f"The percentage of males and females:  {m_perc:,.2%} {f_perc:,.2%}")
 
     """
     ########################################
